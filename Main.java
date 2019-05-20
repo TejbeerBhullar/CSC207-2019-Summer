232cf69043a96a8b9c7f6f70303d7a8cbb47a67d
Tejbeer Singh Bhullar <tejbeerbhullar0001@gmail.com>
2019-05-20 15:16:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
-public class Main {
-  public static void main(String[] args) {
-    Student Jon = new Student();
-    Person Mat = new Person("John Smith");
-    Jon.fullName = "Jon Smith";
-    System.out.println(Jon.fullName); 
+// Lecture 1
+
+
+// In java you must have a main meathod
+// Also everything must be put in a class
+// If I run this I get an error which tells me that I need to implement public static void main with which all
+// classes must be started
+
+public class Main{
+    public static void main(String[] arg){
+
+        int i = 12;
+        String test = "Hello";
+        Person Jon = new Person(); // Using new person is using constructot of a class
+        //Mat.fullName = "Kim";       //2. Aother way is this
+        //System.out.println(Mat); // If I just run this then I get Person@610455d6 which tells us that mAt is a person
+                                        // and also gives us the memory adress
+
+        Jon.fullName = "Jon Smith";
+        System.out.println(Jon.fullName);
     }
- }
+}
+
+
