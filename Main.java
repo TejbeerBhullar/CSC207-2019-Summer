e813468c36415dd7a739f30fc4674e3503165867
Tejbeer Singh Bhullar <tejbeerbhullar0001@gmail.com>
2019-05-20 15:10:10 -0400

    Week 1 Code 1.1

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    Student Jon = new Student();
+    Person Mat = new Person("John Smith");
+    Jon.fullName = "Jon Smith";
+    System.out.println(Jon.fullName); 
+    }
+ }
